d0fcafdb9a1022c55a28bd208049b3f4a09de6a4
saidcoder <saidgrmc16@gmail.com>
2023-08-09 16:01:17 +0700

    add tugas pratikum 2

---
+++ Pratikum-2/Kelipatan_7.go
@@ -0,0 +1,26 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var num int
+// 	var flag bool
+
+// 	fmt.Print("Masukkan Bilangan: ")
+
+// 	_, err := fmt.Scan(&num)
+// 	if err != nil {
+// 		fmt.Println("Input harus angka.")
+// 		return
+// 	}
+
+// 	if num%7 == 0 {
+// 		flag = true
+// 	}
+
+// 	if flag == true {
+// 		fmt.Printf("%d bilangan dari kelipatan 7.\n", num)
+// 	} else {
+// 		fmt.Printf("%d bukan bilangan dari kelipatan 7.\n", num)
+// 	}
+// }
